69b2c3f66d7e618788ff8dbc853f4dd5047f9df5
James Littlefair <jal3g14@soton.ac.uk>
2017-05-17 13:37:18 +0100

    Imported Assets + Began work on traps
    Imported chest, torch, textures
    Made fire trap

---
+++ Assets/Scripts/FireTrap.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrap : MonoBehaviour {
+
+	private GameObject fire;
+	private int i;
+	private ParticleSystem[] emissions;
+	private GameObject fireLight;
+	private bool fireEnabled;
+
+	// Use this for initialization
+	void Start () {
+		fire = this.transform.Find ("FireComplex").gameObject;
+		fireLight = fire.transform.Find ("Light").gameObject;
+		fireEnabled = true;
+		i = 0;
+		emissions = fire.GetComponentsInChildren<ParticleSystem> ();
+	}
+
+	void Update (){
+		i++;
+		if (i == 300) {
+			i = 0;
+			foreach (ParticleSystem e in emissions) {
+				if (fireEnabled) {
+					e.Stop ();
+					//e.gameObject.SetActive(false);
+				} else {
+					e.Play ();
+					//e.gameObject.SetActive(true);
+				}
+			}
+			if (fireEnabled) {
+				fireLight.SetActive (false);
+			} else {
+				fireLight.SetActive (true);
+			}
+			fireEnabled = !fireEnabled;
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.tag == "Player") {
+			
+		}
+	}
+}
