e8362bd1edc667e18c8516e07fcf10a1d3900b86
Alex O'Callaghan <awocallaghan@gmail.com>
2017-05-18 19:17:05 +0100

    Added sound to treasure walkover

--- Assets/Scripts/TreasureWalkover.cs
+++ Assets/Scripts/TreasureWalkover.cs
@@ -6,10 +6,12 @@ public class TreasureWalkover : MonoBehaviour
 {
 
     private GameController gc;
+    private AudioSource audioSource;
 
     void Start()
     {
         gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Increment the score and remove the object when player walks over
@@ -18,6 +20,7 @@ public class TreasureWalkover : MonoBehaviour
         if (collisionInfo.tag == "Player")
         {
             gc.IncrementScore();
+            audioSource.Play();
             GameObject.Destroy(gameObject);
         }
     }
